ce58b53f190f97d5296cecac812b36fe0caf7607
suraj.be <suraj.be@media.net>
2023-06-02 17:49:55 +0530

    Updated postcs.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: [
     require('autoprefixer'),
+    require('postcss-sort-media-queries')('desktop-first'),
     require('cssnano')({
       preset: 'default',
     }),
