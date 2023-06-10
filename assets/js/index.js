4ebcee1a1039e6907739e1609fc4947a98d17655
suraj.be <suraj.be@media.net>
2023-06-10 11:45:51 +0530

    Initial commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,1 @@
-import { handleMenuInteraction } from './menu-interaction'
-import { handleFormValidation } from './form-validation'
-
-const _init = () => {
-  handleMenuInteraction()
-  handleFormValidation()
-  console.log('Working as expected')
-}
-
-document.addEventListener('DOMContentLoaded', _init)
+console.log('Hello')
